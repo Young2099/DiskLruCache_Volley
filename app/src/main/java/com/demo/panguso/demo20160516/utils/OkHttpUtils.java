1496d2986065e44804d6ca299a936152705d7005
yangfang <15868178345@163.com>
2016-06-01 17:22:03 +0800

    OkHttp联网工具

--- app/src/main/java/com/demo/panguso/demo20160516/utils/OkHttpUtils.java
+++ app/src/main/java/com/demo/panguso/demo20160516/utils/OkHttpUtils.java
@@ -1,7 +1,32 @@
 package com.demo.panguso.demo20160516.utils;
 
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+
 /**
- * Created by panguso on 2016/6/1.
+ * Created by yangfang on 2016/6/1.
  */
 public class OkHttpUtils {
+
+    private static OkHttpUtils okHttpUtils;
+    private OkHttpClient okHttpClient;
+    private Request mRequest;
+
+    public static OkHttpUtils getInstance() {
+        if (okHttpUtils == null) {
+            synchronized (OkHttpUtils.class) {
+                if (okHttpUtils == null) {
+                    okHttpUtils = new OkHttpUtils();
+                }
+            }
+        }
+        return okHttpUtils;
+    }
+
+    public void getAscy(String url, Callback callback) {
+        mRequest = new Request.Builder().url(url).build();
+        okHttpClient.newCall(mRequest).enqueue(callback);
+    }
+
 }
